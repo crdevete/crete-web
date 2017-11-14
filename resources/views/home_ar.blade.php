a4fe8fe83a10bb6653881aa8d05d3adcaae2a881
MohammedAli11 <mohammed.alihaggag@gmail.com>
2017-11-14 16:19:02 +0200

    final Changes

--- resources/views/home_ar.blade.php
+++ resources/views/home_ar.blade.php
@@ -46,7 +46,7 @@
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 </head>
-<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top" class="Arabic-body">
 
 <div id="preloader">
     <div class="spinner">
